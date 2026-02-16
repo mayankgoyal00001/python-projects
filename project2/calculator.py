230f67621db2b338ffeceb096e531d646d086889
Mayank Goyal <mayankgoyal587>
2026-02-17 01:10:19 +0530

    calculator first part

---
+++ project2/calculator.py
@@ -0,0 +1,11 @@
+while(True):
+    print("Important Note:- ")
+    print("Enter 1 for calculation :- ")
+    print("Enter 2 for history ")
+    print("Enter 3 for exit")
+    user=int(input("Enter the command\n"))
+    if(user==1):
+        print("Enter 1 for addition")
+        print("Enter 2 for substraction")
+        print("Enter 3 for multiplication");
+        print("Enter 4 for division")
